f6dbe421e8a0406a1cc00d24fc5be1d5a9e2da1f
Vita Kr <vinder_2@mail.ru>
2019-06-21 17:54:18 +0300

    Update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,6 +26,7 @@ author = 'Vita Kr'
 release = __version__
 
 # -- General configuration ---------------------------------------------------
+master_doc = 'index'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
